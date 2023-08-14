adc18602d8f019e618f23a85c8084c8aeac21a55
Shivendra Singh <shiven@alfheimr.tech>
2023-08-14 14:40:46 +0530

    added Makefile

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -6,7 +6,7 @@ import {Script} from "forge-std/Script.sol";
 import {BasicNft} from "../src/BasicNft.sol";
 import {DevOpsTools} from "@foundry-devops/DevOpsTools.sol";
 
-contract Interactions is Script {
+contract MintBasicNft is Script {
     string public constant PUG_URI =
         "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
 
