7263614b98be886f38d0a633512d54c8c1b172ba
Shivendra Singh <shiven@alfheimr.tech>
2023-08-15 11:29:10 +0530

    Created MoodNft

---
+++ README.md
@@ -0,0 +1,76 @@
+# My ERC20 Token 💰
+
+## About
+In this project, I create my own NFT using the ERC-721 token standard and minted them!
+### View contract at:
+[https://sepolia.etherscan.io/address/0xc3fbA630Ca69Ef9b509425227008883A5EA99d09](My NFT on Etherscan)
+
+## Reference
+
+
+## Usage
+### OpenZeppelin
+
+[OpenZeppelin Contracts Docs](https://docs.openzeppelin.com/contracts/4.x/)
+<br><br>
+[OpenZeppelin GitHub Repo](https://github.com/OpenZeppelin/openzeppelin-contracts)
+<br>
+
+### Installing OpenZeppelin Contracts Package
+
+```bash
+forge install OpenZeppelin/openzeppelin-contracts --no-commit
+```
+
+
+## Quickstart 🚀
+
+
+## Testing
+`forge test`
+
+or
+
+`forge test --fork-url $SEPOLIA_RPC_URL`
+
+#### Test coverage
+`forge coverage`
+
+## Deploy
+
+### Start a local node
+
+```
+make anvil
+```
+
+### Deploy on local ANVIL chain ⛓️
+`make deploy`
+### Deploy on Sepolia test net ⛓️
+1. Setup environment variables
+
+You'll want to set your `SEPOLIA_RPC_URL` and `PRIVATE_KEY` as environment variables. You can add them to a `.env` file, similar to what you see in `.env.example`.
+
+- `PRIVATE_KEY`: The private key of your account (like from [metamask](https://metamask.io/)). **NOTE:** FOR DEVELOPMENT, PLEASE USE A KEY THAT DOESN'T HAVE ANY REAL FUNDS ASSOCIATED WITH IT.
+  - You can [learn how to export it here](https://metamask.zendesk.com/hc/en-us/articles/360015289632-How-to-Export-an-Account-Private-Key).
+- `SEPOLIA_RPC_URL`: This is url of the sepolia testnet node you're working with. You can get setup with one for free from [Alchemy](https://alchemy.com/?a=673c802981)
+
+Optionally, add your `ETHERSCAN_API_KEY` if you want to verify your contract on [Etherscan](https://etherscan.io/).
+
+1. Get testnet ETH
+
+Head over to [faucets.chain.link](https://faucets.chain.link/) and get some testnet ETH. You should see the ETH show up in your metamask.
+
+2. Deploy
+
+```
+make deploy ARGS="--network sepolia"
+```
+
+# Thank you! 🤗
+
+If you appreciated this, feel free to follow me:
+
+[![Shivens LinkedIn](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/shivends/)
+
+[![Shivens Twitter](https://img.shields.io/badge/Twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white)](https://twitter.com/shiven_alfheimr)
---
+++ src/MoodNft.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
+
+/**
+@dev 
+Step 1: Getting Image URI as parameters (SVG -> Base64)
+Step 2: Create the Token URI for those image uris (JSON -> Base64 -> String)
+ */
+
+contract MoodNft is ERC721 {
+    enum NFTState {
+        HAPPY,
+        SAD
+    }
+
+    string private s_happySvgUri;
+    string private s_sadSvgUri;
+    uint256 public s_tokenCounter;
+    mapping(uint256 => NFTState) private s_tokenIdToState;
+
+    constructor(
+        string memory happySvgUri,
+        string memory sadSvgUri
+    ) ERC721("Mood NFT", "MN") {
+        s_happySvgUri = happySvgUri;
+        s_sadSvgUri = sadSvgUri;
+        s_tokenCounter = 0;
+    }
+
+    /**
+    Here we are mapping the tokenCounter to the NFTState (and not directly to tokenUri)
+    because the tokenUri will change based on the NFTState (Ref: tokenURI()) for our MoodNFT
+     */
+    function mintNft() public {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenIdToState[s_tokenCounter] = NFTState.HAPPY;
+        s_tokenCounter++;
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "data:application/json;base64,";
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view virtual override returns (string memory) {
+        string memory imageUri;
+
+        if (s_tokenIdToState[tokenId] == NFTState.HAPPY) {
+            imageUri = s_happySvgUri;
+        } else {
+            imageUri = s_sadSvgUri;
+        }
+
+        return
+            string(
+                abi.encodePacked(
+                    _baseURI(),
+                    Base64.encode(
+                        bytes(
+                            abi.encodePacked(
+                                '{"name":"',
+                                name(), // You can add whatever name here
+                                '", "description":"An NFT that reflects the mood of the owner, 100% on Chain!", ',
+                                '"attributes": [{"trait_type": "moodiness", "value": 100}], "image":"',
+                                imageUri,
+                                '"}'
+                            )
+                        )
+                    )
+                )
+            );
+    }
+}
